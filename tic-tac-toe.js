5c892c2fa66d645be0250b4961131a18fc21aff5
sai_varshittha <saivarshitthaponnam7@gmail.com>
2020-07-02 23:44:10 +0530

    updated: all bugs fixed , code working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,195 +1,138 @@
-let player = 'X';
+let gameBoard;
 let computerPlayer = 'O';
-let gameBoard;   
-
+let humanPlayer = 'X';
 let winningCombinations = [ [0,3,6] , [1,4,7] , [2,5,8] , [0,1,2] , [3,4,5] , [6,7,8] , [6,4,2] , [0,4,8] ];
-
 const cells = document.querySelectorAll('.cell');
-
 newGame();
 
-function newGame(){
-
+function newGame() {
 	document.querySelector(".endgame").style.display = "none";
 	gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-	for(let i = 0 ; i < cells.length ; i++){
+	let i = 0 ;
+	while(i < cells.length){
 		cells[i].style.removeProperty('background-color');
 		cells[i].innerText = '';
-		cells[i].addEventListener('click',allowClick,false);
+		cells[i].addEventListener('click', allowClick, false);
+		i++;
 	}
 }
 
-function allowClick(cellInBoard){
-
-	if(typeof gameBoard[cellInBoard.target.id] === 'number'){
-
-		turn(cellInBoard.target.id,player);
-		//alert("xxx");
-		//alert(findBestSpot());
-		//alert("yyy");
-		if(!gameTie()){
-			turn(findBestSpot(),computerPlayer);
-		}
+function allowClick(cellInBoard) {
+	if (typeof gameBoard[cellInBoard.target.id] == 'number') {
+		turn(humanPlayer,cellInBoard.target.id)
+		if (!gameTie() && !checkWin(gameBoard, humanPlayer))
+		 turn(computerPlayer,minimax(gameBoard, computerPlayer).index);
 	}
 }
 
-function turn(cellId,thePlayer){
-		gameBoard[cellId] = thePlayer;
-		document.getElementById(cellId).innerText = thePlayer;
-		let isGameWon = checkWin(gameBoard,thePlayer);
-		if(isGameWon) gameFinished(isGameWon);
-
+function turn(player,cellId) {
+	gameBoard[cellId] = player;
+	document.getElementById(cellId).innerText = player;
+	let isGameWon = checkWin(gameBoard, player)
+	if (isGameWon) gameFinished(isGameWon)
 }
 
-function vacantSquares(){
-
-	let temp = gameBoard.filter( cellVal => typeof cellVal === 'number');
-	return temp;
-}
-
-function checkWin(board,thePlayer){
-
-	let plays = [];
-	for(let i = 0 ; i < board.length ; i++){
-		let temp = board[i];
-		if(temp === thePlayer){
-			plays = plays.concat(i);
-		}
-	}
-
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
 	let gameWon = null;
-
-	for(let [index,win] of winningCombinations.entries()){
-
-		if(win.every(s => plays.indexOf(s) > -1)){
-			gameWon = {index: index, player : thePlayer};
+	for (let [index, win] of winningCombinations.entries()) {
+		if (win.every(s => plays.indexOf(s) > -1)) {
+			gameWon = {index: index, player: player};
 			break;
 		}
-
 	}
-    return gameWon;	
+	return gameWon;
 }
 
-function gameTie(){
-
-	if(vacantSquares().length === 0){
-
-		for(let i = 0 ; i < cells.length ; i++){
+function gameTie() {
+	if (vacantSquares().length == 0) {
+		for (let i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
 			cells[i].removeEventListener('click', allowClick, false);
 		}
-
-		 document.querySelector(".endgame").style.display = "block";
-  		 document.querySelector(".endgame .text").innerText = "Game Tie!";
-		
+		document.querySelector(".endgame").style.display = "block";
+		document.querySelector(".endgame .text").innerText = "Game Tie.";
 		return true;
 	}
-
 	return false;
 }
 
-function gameFinished(gameWon){
-	// have to add code to set the background color based on who won the game
+function gameFinished(gameWon) {
+	for (let index of winningCombinations[gameWon.index]) {
+			if(gameWon.player == humanPlayer){
+				document.getElementById(index).style.backgroundColor = "red";
+			} else if (gameWon.player == computerPlayer){
+				document.getElementById(index).style.backgroundColor = "blue";
+			}
+	}
+	let i = 0 ;
+	while(i < cells.length){
+		cells[i].removeEventListener('click', allowClick, false);
+		i++;
+	}
+	if(gameWon.player == humanPlayer){
+		document.querySelector(".endgame").style.display = "block";
+		document.querySelector(".endgame .text").innerText = "You Won!";
+	} else {
+		document.querySelector(".endgame").style.display = "block";
+		document.querySelector(".endgame .text").innerText = "You lose..";
+	}
+
+}
+
+function vacantSquares() {
+	let filteredList = gameBoard.filter(elem => typeof elem == 'number');
+	return filteredList;
+}
+
+function minimax(board, player) {
+	let emptySlots = vacantSquares();
+	let scoresOfMoves = [];
 	let bestMove;
 
-	for(let i = 0 ; i < cells.length ; i++){
-		cells[i].removeEventListener('click',allowClick,false);
-	}
-
-	if(gameWon.player === player)
-	{
-		
-		 document.querySelector(".endgame").style.display = "block";
-  		 document.querySelector(".endgame .text").innerText = "You won";
-	}
-	else
-		{
-
-		 document.querySelector(".endgame").style.display = "block";
-  		 document.querySelector(".endgame .text").innerText = "You lose";
-
-		}
-}
-/*function evaluate(board,thePlayer,emptySlots){
-
-	if(checkWin(board,thePlayer)){
-		return -10;
-	} else if(checkWin(board,computerPlayer)){
-		return 10;
-	} else if(emptySlots.length === 0){
-		return 0;
-	}
-}*/
-
-function findBestSpot(){
-	return minimax(gameBoard,computerPlayer).index;
-}
-function minimax(board,thePlayer){
-
-	let scoresOfMoves = [];
-
-	let emptySlots = vacantSquares(board);
-	//alert("Heyy");
-	//alert(emptySlots);
-
-/*	//let score = evaluate(board,thePlayer,emptySlots);
-	if(emptySlots.length === 0 || !checkWin(board,player) || !checkWin(board,computerPlayer))
-	{
-		return{score : 0}
-	}
-	else
-	{
-		if(checkWin(board,thePlayer)) return{score : -10};
-		else if(checkWin(board.computerPlayer)) return{score : 10};
-		//checkWin(board,thePlayer)? return{score: -10} : return{score : 10};
-	}
-	
-*/
-	if(checkWin(board,thePlayer)){
+	if (checkWin(board,computerPlayer)) {
+		return {score: 10};
+	} else if (checkWin(board, humanPlayer)) {
 		return {score: -10};
-	}else if(checkWin(board,computerPlayer)){
-		return {score:10};
-	}else if(vacantSquares.length === 0){
-		return {score:0};
+	} else if (emptySlots.length === 0) {
+		return {score: 0};
 	}
-	//alert("heyaa");
-	for(let i = 0 ; i < emptySlots.length ; i++){
 
+	for (let i = 0; i < emptySlots.length; i++) {
 		let tempMove = {};
 		tempMove.index = board[emptySlots[i]];
-		board[emptySlots[i]] = thePlayer;
+		board[emptySlots[i]] = player;
 
-		if(thePlayer === computerPlayer){
-
-			let result = minimax(board,player);
+		if (player == computerPlayer) {
+			let result = minimax(board, humanPlayer);
 			tempMove.score = result.score;
-		}else if(player === player){
-			let result = minimax(board,computerPlayer);
+		} else {
+			let result = minimax(board, computerPlayer);
 			tempMove.score = result.score;
 		}
-		//alert("I came ");
+
 		board[emptySlots[i]] = tempMove.index;
-		//alert(tempMove.index);
 		scoresOfMoves.push(tempMove);
 	}
-	
-	if(player === computerPlayer){
-		let bestScore = -999999;
-		for(let i = 0 ; i < scoresOfMoves.length ; i++){
-			if(scoresOfMoves[i].score > bestScore ){
-				bestScore = scoresOfMoves[i].score;
-				bestMove = i ;
-			}
 
+	if(player === computerPlayer) {
+		let bestScore = -99999;
+		for(let i = 0; i < scoresOfMoves.length; i++) {
+			if (scoresOfMoves[i].score > bestScore) {
+				bestScore = scoresOfMoves[i].score;
+				bestMove = i;
+			}
 		}
 	} else {
-		let bestScore = 999999;
-		for(let i = 0 ; i < scoresOfMoves.length ; i++){
-			if(scoresOfMoves[i].score < bestScore){
+		let bestScore = 99999;
+		for(let i = 0; i < scoresOfMoves.length; i++) {
+			if (scoresOfMoves[i].score < bestScore) {
 				bestScore = scoresOfMoves[i].score;
 				bestMove = i;
 			}
 		}
 	}
-	////alert(scoresOfMoves);
+
 	return scoresOfMoves[bestMove];
 }
