46d778a7be834fa9eebed393a0d3fe39a9f7545c
sai_varshittha <saivarshitthaponnam7@gmail.com>
2020-07-04 16:27:19 +0530

    updated : some button issues

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -23,6 +23,8 @@ function twoPlayerGame(){
 	
 }
 function newGame() {
+	//alert("x");
+	flag = 1;
 	document.querySelector(".endgame").style.display = "none";
 	gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 	let i = 0 ;
